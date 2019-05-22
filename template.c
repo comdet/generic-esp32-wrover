b2aeefc3c4917678b3b6fc89dd466edd35e1e77f
comdet <listzone@hotmail.com>
2019-05-22 15:13:15 +0700

    Update template.c

--- template.c
+++ template.c
@@ -1,18 +1,11 @@
 #include <Arduino.h>
-#include <vector>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 #include <WebServer.h>
-#include "KBProTime.h"
-#include "BluetoothSerial.h"
 
-using namespace std;
+${EXTINC}
 
-KBProTime kbprotime;
-BluetoothSerial SerialBT;
-typedef int Number;
-typedef int Boolean;
 ${VARIABLE}
 
 ${FUNCTION}
